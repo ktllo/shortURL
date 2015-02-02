24450bf3c43ed9441a2bedec888558a0629fdc13
leolo <leolo@leolo.org>
2015-02-02 18:35:39 +0800

    Error file for non existing short URL and disabled short URL

---
+++ info/403.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head><title>ERROR</title></head>
+<body>
+<h1>HTTP 403 - FORBIDDEN</h1>
+<hr>
+<p>The target short URL is disabled.</p>
+<hr>
+<p>Created on <?php date_default_timezone_set('UTC');echo date(DateTime::RFC2822);?></p>
+</html>
