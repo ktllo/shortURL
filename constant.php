a3400496fefaae994e1dc539280ec946338bb592
leolo <leolo@leolo.org>
2015-02-02 16:04:41 +0800

    defines various flags to be used and a almost empty PHP script to test the mod_rewrite

---
+++ constant.php
@@ -0,0 +1,15 @@
+<?php
+
+##User Permission Bits
+define('SU_USER_OP',            0x0001);    #Sys Admin
+define('SU_USER_HOP',           0x0002);    #Half admin
+define('SU_USER_SOP',           0x0003);    #Super admin
+define('SU_USER_ADDURL',        0x0004);    #Able to add URL
+define('SU_USER_APIUSE',        0x0008);    #Can use API
+define('SU_USER_REMOVE',        0x0010);    #Can remove self created link
+define('SU_USER_NOLOG',         0x0020);    #No log for link usage
+
+##URL flags
+define('SU_FLAG_ENABLE',        0x0001);    #URL enabled
+define('SU_FLAG_NOLOG',         0x0002);    #No log for this entry
+define('SU_FLAG_AUTH',          0x0004);    #Authorization needed
