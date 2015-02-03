ba98f927d162dd9434a5fbaa92a2cbb22e2adac8
leolo <leolo@leolo.org>
2015-02-03 12:00:26 +0800

    Updated database

--- db/struct.sql
+++ db/struct.sql
@@ -1,19 +1,46 @@
-CREATE TABLE su_user(
-`uid` INTEGER UNSIGNED PRIMARY KEY,
-`uname` VARCHAR(40) NOT NULL UNIQUE,
-`password` VARCHAR(200) NOT NULL,
-`flags` INTEGER UNSIGNED) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE DATABASE  IF NOT EXISTS `surl` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `surl`;
+-- MySQL dump 10.13  Distrib 5.6.11, for Win32 (x86)
+--
+-- Host: localhost    Database: surl
+-- ------------------------------------------------------
+-- Server version	5.6.13-log
 
-CREATE TABLE `su_user` (
-  `uid` int(10) unsigned NOT NULL,
-  `uname` varchar(40) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `flags` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `uname` (`uname`)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `su_entry`
+--
+
+DROP TABLE IF EXISTS `su_entry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `su_entry` (
+  `id` varchar(200) NOT NULL,
+  `url` text NOT NULL,
+  `flags` int(10) unsigned NOT NULL DEFAULT '1',
+  `created` datetime DEFAULT NULL,
+  `owner` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `su_log`
+--
 
+DROP TABLE IF EXISTS `su_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `su_log` (
   `id` varchar(200) NOT NULL,
   `num` int(11) NOT NULL,
@@ -23,4 +50,32 @@ CREATE TABLE `su_log` (
   `country` char(2) NOT NULL DEFAULT 'XA',
   PRIMARY KEY (`id`,`num`,`datetime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `su_user`
+--
+
+DROP TABLE IF EXISTS `su_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `su_user` (
+  `uid` int(10) unsigned NOT NULL,
+  `uname` varchar(40) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `flags` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `uname` (`uname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-02-03 11:58:38
