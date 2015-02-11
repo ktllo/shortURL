f5b4900a2e34589cc162ccb085defc83cb675e09
leolo <leolo@leolo.org>
2015-02-11 16:41:14 +0800

    Fix improper exported database

--- db/struct.sql
+++ db/struct.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 11, 2015 at 08:21 AM
+-- Generation Time: Feb 11, 2015 at 08:39 AM
 -- Server version: 5.6.13-log
 -- PHP Version: 5.5.3
 
@@ -20,7 +20,6 @@ SET time_zone = "+00:00";
 -- Table structure for table `su_entry`
 --
 
-DROP TABLE IF EXISTS `su_entry`;
 CREATE TABLE IF NOT EXISTS `su_entry` (
   `id` varchar(200) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   `url` text NOT NULL,
@@ -28,6 +27,34 @@ CREATE TABLE IF NOT EXISTS `su_entry` (
   `created` datetime DEFAULT NULL,
   `owner` int(10) unsigned NOT NULL,
   `source` varchar(200) DEFAULT '::0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `su_log`
+--
+
+CREATE TABLE IF NOT EXISTS `su_log` (
+  `id` varchar(200) NOT NULL,
+  `num` int(11) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `source` varchar(200) NOT NULL,
+  `refer` varchar(200) DEFAULT NULL,
+  `country` char(2) NOT NULL DEFAULT 'XA'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `su_user`
+--
+
+CREATE TABLE IF NOT EXISTS `su_user` (
+  `uid` int(10) unsigned NOT NULL,
+  `uname` varchar(40) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `flags` int(10) unsigned DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -39,4 +66,16 @@ CREATE TABLE IF NOT EXISTS `su_entry` (
 --
 ALTER TABLE `su_entry`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `su_log`
+--
+ALTER TABLE `su_log`
+  ADD PRIMARY KEY (`id`,`num`,`datetime`);
+
+--
+-- Indexes for table `su_user`
+--
+ALTER TABLE `su_user`
+  ADD PRIMARY KEY (`uid`), ADD UNIQUE KEY `uname` (`uname`);
 
