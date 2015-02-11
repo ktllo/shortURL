0ac5ca0e6431a1b8548f89e0026b6e7a1279223a
leolo <leolo@leolo.org>
2015-02-11 16:35:09 +0800

    Done adding entry creator ip
    Signed-off-by: leolo <leolo@leolo.org>

--- db/struct.sql
+++ db/struct.sql
@@ -1,81 +1,42 @@
-CREATE DATABASE  IF NOT EXISTS `surl` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `surl`;
--- MySQL dump 10.13  Distrib 5.6.11, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 4.3.9
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: surl
--- ------------------------------------------------------
--- Server version	5.6.13-log
+-- Host: localhost
+-- Generation Time: Feb 11, 2015 at 08:21 AM
+-- Server version: 5.6.13-log
+-- PHP Version: 5.5.3
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `surl`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `su_entry`
 --
 
 DROP TABLE IF EXISTS `su_entry`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `su_entry` (
+CREATE TABLE IF NOT EXISTS `su_entry` (
   `id` varchar(200) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   `url` text NOT NULL,
   `flags` int(10) unsigned NOT NULL DEFAULT '1',
   `created` datetime DEFAULT NULL,
   `owner` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
+  `source` varchar(200) DEFAULT '::0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `su_log`
+-- Indexes for dumped tables
 --
 
-DROP TABLE IF EXISTS `su_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `su_log` (
-  `id` varchar(200) NOT NULL,
-  `num` int(11) NOT NULL,
-  `datetime` datetime NOT NULL,
-  `source` varchar(200) NOT NULL,
-  `refer` varchar(200) DEFAULT NULL,
-  `country` char(2) NOT NULL DEFAULT 'XA',
-  PRIMARY KEY (`id`,`num`,`datetime`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
--- Table structure for table `su_user`
+-- Indexes for table `su_entry`
 --
+ALTER TABLE `su_entry`
+  ADD PRIMARY KEY (`id`);
 
-DROP TABLE IF EXISTS `su_user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `su_user` (
-  `uid` int(10) unsigned NOT NULL,
-  `uname` varchar(40) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `flags` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `uname` (`uname`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-02-04 16:40:55
