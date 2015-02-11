87d676fc5f32c41ac579024b4cace490f0f62e77
leolo <leolo@leolo.org>
2015-02-11 16:45:01 +0800

    Add DROP TABLE

--- db/struct.sql
+++ db/struct.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 11, 2015 at 08:39 AM
+-- Generation Time: Feb 11, 2015 at 08:43 AM
 -- Server version: 5.6.13-log
 -- PHP Version: 5.5.3
 
@@ -20,6 +20,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `su_entry`
 --
 
+DROP TABLE IF EXISTS `su_entry`;
 CREATE TABLE IF NOT EXISTS `su_entry` (
   `id` varchar(200) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   `url` text NOT NULL,
@@ -35,6 +36,7 @@ CREATE TABLE IF NOT EXISTS `su_entry` (
 -- Table structure for table `su_log`
 --
 
+DROP TABLE IF EXISTS `su_log`;
 CREATE TABLE IF NOT EXISTS `su_log` (
   `id` varchar(200) NOT NULL,
   `num` int(11) NOT NULL,
@@ -50,6 +52,7 @@ CREATE TABLE IF NOT EXISTS `su_log` (
 -- Table structure for table `su_user`
 --
 
+DROP TABLE IF EXISTS `su_user`;
 CREATE TABLE IF NOT EXISTS `su_user` (
   `uid` int(10) unsigned NOT NULL,
   `uname` varchar(40) NOT NULL,
