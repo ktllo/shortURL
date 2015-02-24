10fc3b92fff727b8b1a9fce3338a91b98bc6bafc
leolo <leolo@leolo.org>
2015-02-24 10:27:39 +0800

    Add few permssion and access to admin panel

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ define('SU_DB_PREFIX','su_');
 
 define('SU_ID_TYPE',58);//Can be 34,36,58,62
 define('SU_ID_LENGTH',5);//Upto 200
-define('SU_DEFAULT_URL_FLAGS',  0x0001);
+define('SU_DEFAULT_URL_FLAGS',  0x0001);    #Defaulr flags for new entries
 define('SU_USER_DEFAULT',       0x0004);    #Default flags for new users
 define('SU_ANONYMOUS',          0x0000);    #Flags for unidentified user
 define('SU_GEO_DB','includes/GeoLite2-City.mmdb');
@@ -19,3 +19,11 @@ define('SU_BASE_URL','http://localhost/short');
 
 define('SU_DEFAULT_SCHEMA','http');
 $permittedSchema=array('http','https','ftp','irc','ircs');
+
+### Admin-permittion settings
+
+define('SU_ACCESS_NEW_USER',    SU_USER_HOP);
+define('SU_ACCESS_LOCK_USER',   SU_USER_HOP);
+define('SU_ACCESS_REVIEW_URL',  SU_USER_OP);
+define('SU_ACCESS_LOCK_URL',    SU_USER_HOP);
+define('SU_ACCESS_UNLOCK_URL',  SU_DUSE_SOP);
