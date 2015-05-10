7a3dce74c8f73f09f7da773574e99fa7f44dabbb
leolo <leolo@leolo.org>
2015-05-10 12:50:21 +0800

    User may select the id for the short URL they wish to use

--- ajax.php
+++ ajax.php
@@ -14,6 +14,16 @@ if($_POST['action']=='new'){
 <?php
     return;
     }
+    if(empty($_POST['url'])){
+?>
+{
+    "Code" : 480,
+    "Info" : "Malformed request",
+    "Details" : "url required"
+}
+<?php
+        return;
+    }
     $url = processURL($_POST['url']);
     if($url === false){?>
 {
@@ -31,20 +41,54 @@ if($_POST['action']=='new'){
         $idSpace = SU_ID_58;
     else 
         $idSpace = SU_ID_62;
-    //Prepare to add
     $count = 0;
-    while(true){
+    if(isset($_POST['id'])){
+        #Use given ID
+        $id = $_POST['id'];
+        for($i=0;$i<strlen($id);$i++){
+            $char = $id{$i};
+            if(strpos($idSpace,$char) === false){
+?>
+{
+    "Code" : 481,
+    "Info" : "Illegal Character",
+    "id" : "<?php echo $id;?>",
+    "detail" : "Legal characters are <?php echo $idSpace;?>",
+    "violoating" : "<?php echo $id{$i};?>"
+}
+<?php
+                return;
+            }
+        }
+        #ID Looks OK. Attempts to insert
         $stmt = $db->prepare('INSERT INTO '.SU_TABLE_ENTRY.
             ' (`id`,`url`,`flags`,`created`,`owner`,`source`) VALUES'.
             ' (?,?,?,NOW(),?,?);');
-        $id = '';
-        for($i=0;$i<SU_ID_LENGTH;$i++){
-            $id.=$idSpace{rand(0,SU_ID_TYPE-1)};
-        }
         $stmt->execute(array($id,$url,SU_DEFAULT_URL_FLAGS,checkAuth(),$_SERVER['REMOTE_ADDR']));
-        $count++;
-        if($stmt->rowCount()==1){
-            break;
+        if($stmt->rowCount()!=1){
+?>
+{
+    "Code" : "481",
+    "Info" : "ID in use",
+    "id" : "<?php echo $id;?>"
+}
+<?php
+            return;
+        }
+    }else{
+        while(true){
+            $stmt = $db->prepare('INSERT INTO '.SU_TABLE_ENTRY.
+                ' (`id`,`url`,`flags`,`created`,`owner`,`source`) VALUES'.
+                ' (?,?,?,NOW(),?,?);');
+            $id = '';
+            for($i=0;$i<SU_ID_LENGTH;$i++){
+                $id.=$idSpace{rand(0,SU_ID_TYPE-1)};
+            }
+            $stmt->execute(array($id,$url,SU_DEFAULT_URL_FLAGS,checkAuth(),$_SERVER['REMOTE_ADDR']));
+            $count++;
+            if($stmt->rowCount()==1){
+                break;
+            }
         }
     }
 ?>
@@ -52,7 +96,8 @@ if($_POST['action']=='new'){
     "Code"  : 200,
     "id" : "<?php echo $id;?>",
     "long"  : "<?php echo $url?>",
-    "round" : <?php echo $count;?>
+    "round" : <?php echo $count;?>,
+    "short" : "<?php echo SU_BASE_URL."/$id";?>"
 }
 <?php
     return;
